9d103f1367de31a1f1fd0aacdfc21a444de581a8
zilayo <84344709+zilayo@users.noreply.github.com>
2024-02-02 17:37:38 +0000

    feat: add L1 grafana dashboards

--- grafana/provision.sh
+++ grafana/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source_dashboards="$PWD/dashboards/."
+source_dashboards="$PWD/grafana/dashboards/."
 target_dashboards="/etc/grafana/provisioning/dashboards/"
 
 sudo cp -r "$source_dashboards" "$target_dashboards"
@@ -12,5 +12,19 @@ else
     exit 1
 fi
 
-find "$target_dashboards" -name '*.json' -exec sed -i 's/\${DS_PROMETHEUS}/Prometheus-op-stack-docker/g' {} \+
+find "$target_dashboards" -name '*.json' -exec sudo sed -i 's/\${DS_PROMETHEUS_DOCKER}/Prometheus-docker/g' {} \+
+
+find "$target_dashboards" -name '*.json' -exec sudo sed -i 's/\${DS_PROMETHEUS}/Prometheus/g' {} \+
+
+source_datasources="$PWD/grafana/datasources/."
+target_datasources="/etc/grafana/provisioning/datasources/"
+
+sudo cp -r "$source_datasources" "$target_datasources"
+
+if [ $? -eq 0 ]; then
+    echo "Datasources copied successfully."
+else
+    echo "Failed to copy Datasources"
+    exit 1
+fi
 
