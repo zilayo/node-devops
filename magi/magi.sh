c21724ad36fadcfdc8ecfacbfb58a1e4f423b70d
zilayo <84344709+zilayo@users.noreply.github.com>
2024-02-02 22:48:40 +0000

    feat: refactor dockerfiles + build magi from source

--- magi/magi.sh
+++ magi/magi.sh
@@ -4,15 +4,17 @@ set -e
 L1_RPC_URL="http://host.docker.internal:8545"
 EXECUTION_CLIENT="op-reth"
 RPC_PORT=9645
+RPC_ADDR="0.0.0.0"
 SYNC_MODE="full"
 
-magi \
-    --network "./rollup.json" \
+exec /usr/local/bin/magi \
+    --network "$CHAIN_NAME" \
     --jwt-secret $(cat $JWT_PATH) \
     --l1-rpc-url $L1_RPC_URL \
     --l2-rpc-url http://${EXECUTION_CLIENT}:9545 \
     --l2-engine-url http://${EXECUTION_CLIENT}:9551 \
-    --rpc-port $RPC_PORT \
-    --sync-mode $SYNC_MODE \
+    --rpc-port "$RPC_PORT" \
+    --rpc-addr "$RPC_ADDR" \
+    --sync-mode "$SYNC_MODE" \
     -v \
     --logs-dir /root/magilogs/
